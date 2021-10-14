7daa22977a9830114ce8409de0bfba5ce249976e
DaniloGuerrero <dajogere@gmail.com>
2021-10-13 21:51:20 -0500

    Creaccion del Layout.html V1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('Layout.html')
+    return render_template('Login.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
