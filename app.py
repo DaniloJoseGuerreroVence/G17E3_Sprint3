c88f6685c40e6881a1a66d44b3bc064d882444d5
mmaltamar05 <mmaltamar05@gmail.com>
2021-10-21 20:32:48 -0500

    add product charged

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('Login.html')
 
+@app.route('/AddProduct')
+def AddProduct():
+    return render_template('add_product.html')
+
+
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
  
