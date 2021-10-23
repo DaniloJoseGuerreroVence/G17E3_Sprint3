d94c4e8a11f655e4d88d771c0d0ca6b3694655a7
DaniloGuerrero <dajogere@gmail.com>
2021-10-22 19:33:17 -0500

    Se modifica layout para adicionar barra de busqueda y se edita css StyleBasic para qeu no interfiera con el despliegue de las otras paginas

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/Home')
 def index():
     return render_template('Login.html')
+
+
+@app.route('/buscar')
+def buscar():
+    return "<H1>BUSCANDO...</H1>"
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
